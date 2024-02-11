5651d09431ccdfe9890c9e13d5e69bfce652550c
Yigit Yalcin <yigityalciin@hotmail.com>
2024-02-11 17:57:33 +0300

    Add project files.

---
+++ Workaround/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace Workaround;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Degiskenler();
+        Vatandas vatandas = new();
+        
+        
+    }
+
+    private static void Degiskenler()
+    {
+        string mesaj = "Merhaba";
+        double tutar = 100000; //dbden gelir bu bilgiler.
+        int sayi = 100;
+        bool girisYapmisMi = false;
+
+        string ad = "Yigit";
+        string soyad = "Yalcin";
+        int dogumYili = 1985;
+        long tcNo = 22111666999;
+
+        Console.WriteLine(tutar * 1.18);
+    }
+}
+   
+public class Vatandas
+{
+    public string Ad { get; set; }
+    public string Soyad { get; set; }
+    public int DogumYili { get; set; }
+    public long TcNo { get; set; }
+}
